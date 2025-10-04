bbef257eac865d8399ff00ce5abcf53c76650301
Joanseco <joanseco5@gmail.com>
2025-10-04 17:53:56 -0500

    try

--- database.js
+++ database.js
@@ -1,22 +1,21 @@
 const { Pool } = require('pg');
 
 async function connectToDatabase() {
-    const pool = new Pool({
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false
-        }
-    });
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
 
-    try {
-        const client = await pool.connect();
-        client.release();
-        console.log('✅ Conexión exitosa a PostgreSQL.');
-        return pool;
-    } catch (error) {
-        console.error('❌ Error al conectar con PostgreSQL:', error);
-        throw error;
-    }
+  try {
+    await pool.connect();
+    console.log('✅ Conexión exitosa a PostgreSQL.');
+    return pool;
+  } catch (error) {
+    console.error('❌ Error al conectar con PostgreSQL:', error.message);
+    throw error;
+  }
 }
 
 module.exports = { connectToDatabase };
