99edb0d4bdd37c5d24e99da29904abad24524630
Joanseco <joanseco5@gmail.com>
2025-10-04 18:16:03 -0500

    cambio port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const { connectToDatabase } = require('./database.js');
 
 const app = express();
-const PORT = process.env.API_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 let db;
 
